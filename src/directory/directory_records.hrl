c0d61aa4681bfa2a3feaecfc7d1ee390b2730fcd
Florian Unterberger <funterberger@gmail.com>
2015-11-16 03:18:26 +0100

    adding most basic directory server (retrieving users by username)

---
+++ src/directory/directory_records.hrl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%%% @author Florian
+%%% @copyright (C) 2015, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 16. Nov 2015 00:30
+%%%-------------------------------------------------------------------
+-author("Florian").
+
+%% records -----------------------------------------------------------
+%% field 1 = table name, field 2 = key, field 3 = field 1
+-record(ibo_user, {
+    username :: nonempty_string(),
+    firstname :: nonempty_string(),
+    lastname :: nonempty_string(),
+    groups = [] :: nonempty_list(nonempty_string())}).
+
---
+++ src/directory/directory_server.erl
@@ -0,0 +1,59 @@
+%%%-------------------------------------------------------------------
+%%% @author Florian
+%%% @copyright (C) 2015, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 16. Nov 2015 00:24
+%%%-------------------------------------------------------------------
+-module(directory_server).
+-author("Florian").
+
+-include("directory_records.hrl").
+
+%% gen_server --------------------------------------------------------
+-behaviour(gen_server).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+    terminate/2, code_change/3]).
+
+%% specs -------------------------------------------------------------
+-spec get_user(nonempty_string()) -> #ibo_user{}.
+
+%% API ---------------------------------------------------------------
+-export([start_link/0, get_user/1]).
+
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+get_user(Username) ->
+    gen_server:call(?MODULE, {get_user, Username}).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+init([]) ->
+    %% Note we must set trap_exit = true if we
+    %% want terminate/2 to be called when the application
+    %% is stopped
+    process_flag(trap_exit, true),
+    io:format("~p starting~n", [?MODULE]),
+    {ok, 0}.
+handle_call({get_user, Username}, _From, N) ->
+    {reply, read_transactional(ibo_user,Username), N + 1}.
+handle_cast(_Msg, N) -> {noreply, N}.
+handle_info(_Info, N) -> {noreply, N}.
+terminate(_Reason, _N) ->
+    io:format("~p stopping~n", [?MODULE]),
+    ok.
+code_change(_OldVsn, N, _Extra) -> {ok, N}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+%%do(qlc:q([X || X <- mnesia:table(ibo_directory), X#user.username =:= Username])).
+
+read_transactional(Table,Key) ->
+    F = fun() ->
+        mnesia:read(Table,Key)
+        end,
+    mnesia:transaction(F).
