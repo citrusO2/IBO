b2e6b2bf8629f0b4858d575014fe7ed0f727dc52
Florian Unterberger <funterberger@gmail.com>
2015-11-16 03:16:03 +0100

    adding basic install function for mnesia database

--- src/ibo_app.erl
+++ src/ibo_app.erl
@@ -1,15 +1,37 @@
+%%%-------------------------------------------------------------------
+%%% @author Florian
+%%% @copyright (C) 2015, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 13. Nov 2015 22:47
+%%%-------------------------------------------------------------------
 -module(ibo_app).
+-author("Florian").
 
+-include("directory/directory_records.hrl").
+
+%% application -------------------------------------------------------
 -behaviour(application).
-
-%% Application callbacks
 -export([start/2, stop/1]).
 
+%% API ---------------------------------------------------------------
+-export([install/1]).
+
+install(Nodes) ->
+    ok = mnesia:create_schema(Nodes),
+    rpc:multicall(Nodes, application, start, [mnesia]),
+    mnesia:create_table(ibo_user,
+        [{attributes, record_info(fields, ibo_user)},
+            {disc_copies, Nodes},
+            {type, set}]),
+    rpc:multicall(Nodes, application, stop, [mnesia]).
+
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
-
 start(_StartType, _StartArgs) ->
+    mnesia:wait_for_tables([ibo_directory],5000),
     ibo_sup:start_link().
 
 stop(_State) ->
