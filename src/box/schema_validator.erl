c089fb2a935125de802768617c93b139308b8b48
Florian Unterberger <funterberger@gmail.com>
2015-12-09 22:54:59 +0100

    updating schema_validator to respond with error messages if validation fails + renaming function name so that a possible boolean response type is no longer indicated

--- src/box/schema_validator.erl
+++ src/box/schema_validator.erl
@@ -9,101 +9,118 @@
 -module(schema_validator).
 -author("Florian").
 
+-define(CT(ThrowReason, ProblemValue, Expression),
+    case Expression of true -> true;_ -> throw({ThrowReason, ProblemValue}) end).
+
 %% API
--export([is_valid_schema/1, is_valid_data/2, validate_schema_and_data/2]).
+-export([validate_schema/1, validate_data/2, validate_schema_and_data/2]).
 
-is_valid_schema(Schema) ->
-    validate_map(Schema).
-
-validate_schema_and_data(Schema, Data) ->
-    case is_valid_schema(Schema) of
-         true ->
-             validate(Schema,Data);
-         false ->
-             {error, not_valid_schema}
+validate_schema(Schema) ->
+    try validate_map(Schema) of
+        true ->
+            {ok, Schema}
+    catch
+        _:Error ->
+            {error, Error}
     end.
 
-is_valid_data(Schema, Data) ->
-    validate(Schema, Data).
+validate_schema_and_data(Schema, Data) ->
+    case validate_schema(Schema) of
+        {ok, _} ->
+            validate(Schema, Data);
+        {error, Error} ->
+            {error, Error}
+    end.
+
+validate_data(Schema, Data) ->
+    try validate(Schema, Data) of
+        true ->
+            {ok, Data}
+    catch
+        _:Error ->
+            {error, Error}
+    end.
 
 %% Validating Schema -------------------------------------------------
 %CMap = current map = parent map of current element
 validate_keyvalue(<<"title">>, Title, _CMap) ->
-    is_binary(Title);
+    ?CT("title has to be of type binary", Title, is_binary(Title));
 validate_keyvalue(<<"description">>, Description, _CMap) ->
-    is_binary(Description);
+    ?CT("description has to be of type binary", Description, is_binary(Description));
 validate_keyvalue(<<"type">>, Type, _CMap) ->
-    lists:member(Type, [<<"object">>, <<"string">>, <<"number">>, <<"boolean">>, <<"array">>, <<"null">>]);
+    ?CT("type has to be one of the following: object, string, number, boolean, array, null", Type,
+        lists:member(Type, [<<"object">>, <<"string">>, <<"number">>, <<"boolean">>, <<"array">>, <<"null">>]));
 validate_keyvalue(<<"required">>, List, CMap) ->
-    is_list(List) andalso
-        lists:all(fun(E) -> is_binary(E) end, List) andalso
-        lists:all(fun(E) -> lists:member(E, List) end, maps:keys(maps:get(<<"properties">>, CMap)));    % required has to match the keys in properties
+    ?CT("required has to be of type list", List, is_list(List)) andalso
+        ?CT("required elements have to be of type binary", List, lists:all(fun(E) -> is_binary(E) end, List)) andalso
+        ?CT("required elements must match the keys in properties", List, lists:all(fun(E) ->
+            lists:member(E, List) end, maps:keys(maps:get(<<"properties">>, CMap))));    % required has to match the keys in properties
 validate_keyvalue(<<"enum">>, List, _CMap) ->
-    is_list(List) andalso lists:all(fun(E) -> is_binary(E) end, List);
+    ?CT("enum has to be of type list", List, is_list(List)) andalso
+        ?CT("enum elements have to be of type binary", List, lists:all(fun(E) -> is_binary(E) end, List));
 validate_keyvalue(Key, Map, _CMap) when is_map(Map) ->
     validate_map(Map, Key).
 
 validate_map(Map) when is_map(Map) ->
-    ((maps:get(<<"type">>,Map) =:= <<"object">> andalso maps:is_key(<<"properties">>,Map) andalso not maps:is_key(<<"items">>,Map)) orelse
-    (maps:get(<<"type">>,Map) =:= <<"array">> andalso maps:is_key(<<"items">>,Map) andalso not maps:is_key(<<"properties">>,Map)) orelse
-    (lists:member(maps:get(<<"type">>,Map),[<<"string">>, <<"number">>, <<"boolean">>,<<"null">>]) andalso not maps:is_key(<<"items">>,Map) andalso not maps:is_key(<<"properties">>,Map)))
+    ?CT("the given map must be either of (type=object, properties, no items), (type=aray, items, no properties) or (type string/number/boolean/null, no items, no properties", Map,
+        ((maps:get(<<"type">>, Map) =:= <<"object">> andalso maps:is_key(<<"properties">>, Map) andalso not maps:is_key(<<"items">>, Map)) orelse
+            (maps:get(<<"type">>, Map) =:= <<"array">> andalso maps:is_key(<<"items">>, Map) andalso not maps:is_key(<<"properties">>, Map)) orelse
+            (lists:member(maps:get(<<"type">>, Map), [<<"string">>, <<"number">>, <<"boolean">>, <<"null">>]) andalso not maps:is_key(<<"items">>, Map) andalso not maps:is_key(<<"properties">>, Map))))
         andalso maps:fold(fun(K, Val, AccIn) -> AccIn andalso validate_keyvalue(K, Val, Map) end, true, Map).
 
 validate_map(Map, <<"properties">>) when is_map(Map) ->
-    maps:fold(fun(K, Val, AccIn) -> AccIn andalso validate_keyvalue(K, Val, Map) end, true, Map);
+    maps:fold(fun(K, Val, AccIn) ->
+        AccIn andalso validate_keyvalue(K, Val, Map) end, true, Map); % TODO: more checks here, because property-map must contain more objects and not any reserved keywort, e.g. description or title
 validate_map(Map, _Key) when is_map(Map) ->
     validate_map(Map).
 
 %% Validate Data against the Schema ----------------------------------
 validate(Schema, ValuesList) when is_list(ValuesList) ->
-    case maps:get(<<"type">>, Schema) =:= <<"array">> of
+    case ?CT("the given value is a list, but the schema is not of type array", {Schema, ValuesList}, maps:get(<<"type">>, Schema) =:= <<"array">>) of
         true ->
-            SubSchema = maps:get(<<"items">>,Schema),
-            lists:foldl(fun(Value,Acc) -> Acc andalso validate(SubSchema, Value) end, true, ValuesList);
-        false ->
-            false
+            SubSchema = maps:get(<<"items">>, Schema),
+            lists:foldl(fun(Value, Acc) -> Acc andalso validate(SubSchema, Value) end, true, ValuesList)
     end;
 validate(Schema, ValuesMap) when is_map(ValuesMap) ->
-    case maps:get(<<"type">>, Schema) =:= <<"object">> of
+    case ?CT("the given value is a map, but the schema is not of type object", {Schema, ValuesMap}, maps:get(<<"type">>, Schema) =:= <<"object">>) of
         true ->
             RequiredList = maps:get(<<"required">>, Schema),
             PropertiesMap = maps:get(<<"properties">>, Schema),
 
             case validate_required(RequiredList, ValuesMap) andalso validate_values_in_properties(PropertiesMap, ValuesMap) of
                 true ->
-                    maps:fold(fun(K,Val,AccIn) -> AccIn andalso validate(maps:get(K,PropertiesMap), Val) end, true, ValuesMap);
+                    maps:fold(fun(K, Val, AccIn) ->
+                        AccIn andalso validate(maps:get(K, PropertiesMap), Val) end, true, ValuesMap);
                 false ->
                     false
-            end;
-        false ->
-            false
+            end
     end;
 validate(Schema, Value) when is_binary(Value) ->
-    case maps:get(<<"type">>, Schema) =:= <<"string">> of
+    case ?CT("the given value is of type binary, but the schema is not of type string", {Schema, Value}, maps:get(<<"type">>, Schema) =:= <<"string">>) of
         true ->
-            case maps:is_key(<<"enum">>,Schema) of
+            case maps:is_key(<<"enum">>, Schema) of
                 true ->
-                    lists:member(Value, maps:get(<<"enum">>,Schema));
+                    ?CT("The given value differs from the values in enum", {Schema, Value}, lists:member(Value, maps:get(<<"enum">>, Schema)));
                 false ->
                     true
-            end;
-        false ->
-            false
+            end
     end;
 validate(Schema, Value) when is_integer(Value) ->
-    maps:get(<<"type">>, Schema) =:= <<"integer">>;
+    ?CT("the given value is of type integer, but the schema is not of type integer", {Schema, Value}, maps:get(<<"type">>, Schema) =:= <<"integer">>);
 validate(Schema, Value) when is_number(Value) ->
-    maps:get(<<"type">>, Schema) =:= <<"number">>;
-validate(Schema, Value) when is_boolean(Value)->
-    maps:get(<<"type">>, Schema) =:= <<"boolean">>;
+    ?CT("the given value is of type number, but the schema is not of type number", {Schema, Value}, maps:get(<<"type">>, Schema) =:= <<"number">>);
+validate(Schema, Value) when is_boolean(Value) ->
+    ?CT("the given value is of type boolean, but the schema is not of type boolean", {Schema, Value}, maps:get(<<"type">>, Schema) =:= <<"boolean">>);
 validate(Schema, null) ->
-    maps:get(<<"type">>, Schema) =:= <<"null">>.
+    ?CT("the given value is of type null, but the schema is not of type null", {Schema, null}, maps:get(<<"type">>, Schema) =:= <<"null">>).
 
-validate_required(RequiredList,ValuesMap)->
+validate_required(RequiredList, ValuesMap) ->
     ValueKeys = maps:keys(ValuesMap),
-    lists:all(fun(E) -> lists:member(E, ValueKeys) end, RequiredList).
+    ?CT("the given value-map does not contain all required keys", {RequiredList, ValuesMap}, lists:all(fun(E) ->
+        lists:member(E, ValueKeys) end, RequiredList)).
 
 validate_values_in_properties(PropertiesMap, ValuesMap) ->
     ValueKeys = maps:keys(ValuesMap),
-    PropertyKeys =  maps:keys(PropertiesMap),
-    lists:all(fun(E) -> lists:member(E, PropertyKeys) end, ValueKeys).
+    PropertyKeys = maps:keys(PropertiesMap),
+    ?CT("the given value-map contains more properties than defined in the properties-map", {PropertiesMap, ValuesMap}, lists:all(fun(E) ->
+        lists:member(E, PropertyKeys) end, ValueKeys)).
--- test/schema_validator_SUITE.erl
+++ test/schema_validator_SUITE.erl
@@ -14,7 +14,7 @@
 %% Common Test Framework ---------------------------------------------
 -include_lib("common_test/include/ct.hrl"). % enables ?config(Key, List) to retrieve properties from the Config
 -export([all/0, init_per_testcase/2, end_per_testcase/2, init_per_suite/1, end_per_suite/1]).
--export([validate_schema_test/1,validate_schema_data_test/1]).
+-export([validate_schema_test/1, validate_schema_data_test/1]).
 
 all() -> [validate_schema_test, validate_schema_data_test].
 
@@ -34,43 +34,68 @@ end_per_testcase(_, _Config) ->
 %%% Validation Tests
 %%%===================================================================
 
-validate_schema_test(_Config)->
-    true = schema_validator:is_valid_schema(?TESTSCHEMA1),
-    false = schema_validator:is_valid_schema(?TESTSCHEMA2),
-    false = schema_validator:is_valid_schema(?TESTSCHEMA3),
-    false = schema_validator:is_valid_schema(?TESTSCHEMA4),
-    false = schema_validator:is_valid_schema(?TESTSCHEMA5),
-    false = schema_validator:is_valid_schema(?TESTSCHEMA6),
-    false = schema_validator:is_valid_schema(?TESTSCHEMA7),
-    true = schema_validator:is_valid_schema(?TESTSCHEMA8),
-    false = schema_validator:is_valid_schema(?TESTSCHEMA9),
-    false = schema_validator:is_valid_schema(?TESTSCHEMA10),
-    false = schema_validator:is_valid_schema(?TESTSCHEMA11).
+validate_schema_test(_Config) ->
+    TS1 = ?TESTSCHEMA1,
+    TS2 = ?TESTSCHEMA2,
+    TS3 = ?TESTSCHEMA3,
+    TS4 = ?TESTSCHEMA4,
+    TS5 = ?TESTSCHEMA5,
+    TS6 = ?TESTSCHEMA6,
+    TS7 = ?TESTSCHEMA7,
+    TS8 = ?TESTSCHEMA8,
+    TS9 = ?TESTSCHEMA9,
+    TS10 = ?TESTSCHEMA10,
+    TS11 = ?TESTSCHEMA11,
 
-validate_schema_data_test(_Config)->
+    {ok, TS1} = schema_validator:validate_schema(TS1),
+    {error, {"description has to be of type binary", 2312}} = schema_validator:validate_schema(TS2),
+    {error, {"the given map must be either of (type=object, properties, no items), (type=aray, items, no properties) or (type string/number/boolean/null, no items, no properties", TS3}} = schema_validator:validate_schema(TS3),
+    {error, {"title has to be of type binary", true}} = schema_validator:validate_schema(TS4),
+    {error, {"enum elements have to be of type binary", [3, <<"yes">>, <<"maybe">>]}} = schema_validator:validate_schema(TS5),
+    {error, {"required elements must match the keys in properties", [<<"wuhaha">>, <<"yesno">>]}} = schema_validator:validate_schema(TS6),
+    {error, {"the given map must be either of (type=object, properties, no items), (type=aray, items, no properties) or (type string/number/boolean/null, no items, no properties", TS7}} = schema_validator:validate_schema(TS7),
+    {ok, TS8} = schema_validator:validate_schema(TS8),
+    {error, {"the given map must be either of (type=object, properties, no items), (type=aray, items, no properties) or (type string/number/boolean/null, no items, no properties", TS9}} = schema_validator:validate_schema(TS9),
+    {error, {"the given map must be either of (type=object, properties, no items), (type=aray, items, no properties) or (type string/number/boolean/null, no items, no properties", TS10}} = schema_validator:validate_schema(TS10),
+    {error, {"the given map must be either of (type=object, properties, no items), (type=aray, items, no properties) or (type string/number/boolean/null, no items, no properties", TS11}} = schema_validator:validate_schema(TS11).
+
+validate_schema_data_test(_Config) ->
     Data1 = #{<<"reason">> => <<"DecisionReason">>, <<"yesno">> => <<"yes">>},
-    true = schema_validator:is_valid_data(?TESTSCHEMA1,Data1),
+    {ok, Data1} = schema_validator:validate_data(?TESTSCHEMA1, Data1),
 
     Data2 = #{<<"reason">> => <<"DecisionReason">>, <<"yesno">> => <<"haha">>},
-    false = schema_validator:is_valid_data(?TESTSCHEMA1,Data2),
+    ResSchema2 = #{<<"description">> => <<"tick your decision">>,
+        <<"enum">> => [<<"no">>, <<"yes">>, <<"maybe">>],
+        <<"title">> => <<"Decide">>,
+        <<"type">> => <<"string">>},
+    {error, {"The given value differs from the values in enum", {ResSchema2,
+        <<"haha">>}}} = schema_validator:validate_data(?TESTSCHEMA1, Data2),
 
     Data3 = #{<<"yesno">> => <<"yes">>},
-    false = schema_validator:is_valid_data(?TESTSCHEMA1,Data3),
+    {error, {"the given value-map does not contain all required keys", {[<<"reason">>, <<"yesno">>], Data3}}} = schema_validator:validate_data(?TESTSCHEMA1, Data3),
 
     Data4 = #{<<"reason">> => <<"DecisionReason">>},
-    false = schema_validator:is_valid_data(?TESTSCHEMA1,Data4),
+    {error, {"the given value-map does not contain all required keys", {[<<"reason">>, <<"yesno">>], Data4}}} = schema_validator:validate_data(?TESTSCHEMA1, Data4),
 
     Data5 = #{<<"reason">> => 25, <<"yesno">> => <<"yes">>},
-    false = schema_validator:is_valid_data(?TESTSCHEMA1,Data5),
+    ResSchema5 = #{<<"description">> => <<"The reason for your decision">>,
+        <<"title">> => <<"Reason">>,
+        <<"type">> => <<"string">>},
+    {error, {"the given value is of type integer, but the schema is not of type integer", {ResSchema5, 25}}} = schema_validator:validate_data(?TESTSCHEMA1, Data5),
 
+    ResSchema6 = ResSchema5,
     Data6 = #{<<"reason">> => true, <<"yesno">> => <<"yes">>},
-    false = schema_validator:is_valid_data(?TESTSCHEMA1,Data6),
+    {error, {"the given value is of type boolean, but the schema is not of type boolean", {ResSchema6, true}}} = schema_validator:validate_data(?TESTSCHEMA1, Data6),
 
+    ResSchema7 = ResSchema5,
     Data7 = #{<<"reason">> => [<<"DecisionReason">>], <<"yesno">> => <<"yes">>},
-    false = schema_validator:is_valid_data(?TESTSCHEMA1,Data7),
+    {error, {"the given value is a list, but the schema is not of type array", {ResSchema7, [<<"DecisionReason">>]}}} = schema_validator:validate_data(?TESTSCHEMA1, Data7),
 
+    ResSchema8 = ResSchema5,
     Data8 = #{<<"reason">> => null, <<"yesno">> => <<"yes">>},
-    false = schema_validator:is_valid_data(?TESTSCHEMA1,Data8),
+    {error, {"the given value is of type null, but the schema is not of type null", {ResSchema8, null}}} = schema_validator:validate_data(?TESTSCHEMA1, Data8),
 
     Data9 = #{<<"reason">> =>#{<<"properties">> => <<"DecisionReason">>}, <<"yesno">> => <<"yes">>},
-    false = schema_validator:is_valid_data(?TESTSCHEMA1,Data9).
+    Value9 = #{<<"properties">> => <<"DecisionReason">>},
+    ResSchema9 = ResSchema5,
+    {error, {"the given value is a map, but the schema is not of type object", {ResSchema9, Value9}}} = schema_validator:validate_data(?TESTSCHEMA1, Data9).
