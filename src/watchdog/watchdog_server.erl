3ff8926b7b71558d1b825fb1079052a4b38d2c6d
Florian Unterberger <funterberger@gmail.com>
2016-04-05 19:01:15 +0200

    adding watchdog which enables the dynamical adding and removing of supervised iactors to an erlang node

--- src/watchdog/watchdog_server.erl
+++ src/watchdog/watchdog_server.erl
@@ -11,7 +11,8 @@
 
 %% watchdog_server internal state ----------------------------------------
 -record(state, {
-    test :: pid()     % testinternal state
+    iactors :: list( {atom(), Args :: term() } )  ,   % list of actors to start at the beginning with their initialisation values
+    filename = watchdog_configuration :: atom()
 }).
 
 %% gen_server --------------------------------------------------------
@@ -21,13 +22,16 @@
 
 %% API
 -export([start_link/0,
-    start_iactor/1, stop_iactor/1]).
+    start_iactor/1, start_iactor/2, stop_iactor/1]).
 
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 start_iactor(Name) ->
-    gen_server:call(?MODULE, {start_iactor, Name}).
+    start_iactor(Name, []).
+
+start_iactor(Name, Args) ->
+    gen_server:call(?MODULE, {start_iactor, Name, Args}).
 
 stop_iactor(Name) ->
     gen_server:call(?MODULE, {stop_iactor, Name}).
@@ -36,24 +40,35 @@ stop_iactor(Name) ->
 %%% gen_server callbacks
 %%%===================================================================
 init([]) ->
-    % TODO: load info from database, so that the modules from before are added again
     process_flag(trap_exit, true),
     io:format("~p starting~n", [?MODULE]),
-    {ok, #state{}}. % 0 = initial state
+    S = #state{},
 
-handle_call({start_iactor, Name}, _From, State) ->
-    % TODO: store info, which modules to start in database
-    {reply, add_iactor_to_supervisor(Name), State};
-handle_call({stop_iactor, Name}, _From, State) ->
-    % TODO: update info of modules to start
-    {reply, remove_iactor_from_supervisor(Name), State};
+    % open settings from disk or create new settings on disk & start stored actors
+    {ok, _} = dets:open_file(S#state.filename, []),
+    Iactors = dets:foldl(fun({Name, Args} = Iactor, AccIn) ->
+        ok = add_iactor_to_supervisor(Name,Args),
+        [Iactor|AccIn] end,[], S#state.filename),
+    {ok, S#state{iactors = Iactors}}.
+
+handle_call({start_iactor, Name, Args}, _From, S) ->
+    ok = add_iactor_to_supervisor(Name, Args),  % try to start iactor first, otherwise a wrong call here would crash the server, which would then try to start the iactor again when restarting with the same errornous setings
+    ok = dets:insert(S#state.filename,{Name, Args}),
+    NewState = S#state{iactors = [{Name,Args}|S#state.iactors]},
+    {reply, ok, NewState};
+handle_call({stop_iactor, Name}, _From, S) ->
+    ok = remove_iactor_from_supervisor(Name),
+    ok = dets:delete(S#state.filename, Name),
+    NewState = S#state{iactors = lists:keydelete(Name, 1, S#state.iactors)},
+    {reply, ok, NewState};
 handle_call(stop, _From, State) ->
     {stop, normal, stopped, State}.
 
 handle_cast(_Msg, State) -> {noreply, State}.
 handle_info(_Info, N) -> {noreply, N}.
-terminate(_Reason, _N) ->
+terminate(_Reason, S) ->
     io:format("~p stopping~n", [?MODULE]),
+    dets:close(S#state.filename),
     ok.
 code_change(_OldVsn, N, _Extra) -> {ok, N}.
 
@@ -64,11 +79,10 @@ code_change(_OldVsn, N, _Extra) -> {ok, N}.
 %%%===================================================================
 %%% Dynamic IBO actor adding / removing
 %%% * iactor = actor's supervisor which starts the actual IBO actor
-%%% * private so that only the watchdog_server can access the pool directly!
 %%%===================================================================
-add_iactor_to_supervisor(SupervisorName) ->
+add_iactor_to_supervisor(SupervisorName, Args) ->
     ActorSpec = #{id => SupervisorName,
-        start => {SupervisorName, start_link, []},
+        start => {SupervisorName, start_link, Args},
         restart => transient,   % warning: when two nodes start the same global iactor -> restart crashes possible
         shutdown => 10000,  % = 10secs, time in milliseconds for shutdown before child is brutally killed
         type => supervisor,
--- test/watchdog_server_SUITE.erl
+++ test/watchdog_server_SUITE.erl
@@ -12,9 +12,9 @@
 %% Common Test Framework ---------------------------------------------
 -include_lib("common_test/include/ct.hrl"). % enables ?config(Key, List) to retrieve properties from the Config
 -export([all/0, init_per_testcase/2, end_per_testcase/2, init_per_suite/1, end_per_suite/1]).
--export([start_iactor_test/1, iactor_restart_test/1, iactor_restart_test2/1, iactor_restart_test3/1]).
+-export([start_iactor_test/1, iactor_restart_test/1, iactor_restart_test2/1, iactor_restart_test3/1, iactor_config_test/1]).
 
-all() -> [start_iactor_test, iactor_restart_test, iactor_restart_test2, iactor_restart_test3 ].
+all() -> [start_iactor_test, iactor_restart_test, iactor_restart_test2, iactor_restart_test3, iactor_config_test ].
 
 init_per_suite(Config) ->
     Config.
@@ -23,21 +23,11 @@ end_per_suite(_Config) ->
     ok.
 
 init_per_testcase(_, Config) -> % first argument = name of the testcase as atom, Config = Property list
-    {'ok', Pid} = watchdog_sup:start_link(),
-    [{sup,Pid} | Config].
+    start_watchdog_processes(Config).
 
 end_per_testcase(_, Config) ->
-    Sup = ?config(sup,Config),
-    process_flag(trap_exit, true),  % need to trap the exit of the supervisor, otherwise the test-process dies as well!
-    Ref = erlang:monitor(process,Sup),
-    erlang:exit(Sup,shutdown),  % stop the watchdog's supervisor and wait till everything is down, also see: http://stackoverflow.com/questions/21138442/stopping-an-erlang-supervisor
-    receive
-        {'DOWN', Ref, process, Sup, _Reason} ->
-            ok
-    after 1000 ->
-        error(exit_timeout)
-    end,
-    process_flag(trap_exit, false).
+    dets:delete_all_objects(watchdog_configuration),    % persistend configuration for the watchdog need to be deleted, otherwise one tests affects the next one
+    stop_watchdog_processes(Config).
 
 %%%===================================================================
 %%% User Tests
@@ -63,7 +53,7 @@ iactor_restart_test(_Config) -> % when an iactor is stopped, it should be restar
     ct_helper:wait(),
     true = is_registered(repo_server),
     NewRepoPid = whereis(repo_server),
-    RepoPid =/= NewRepoPid,
+    RepoPid /= NewRepoPid,
     ok.
 
 iactor_restart_test2(_Config) ->    % when an iactor is killed, it should be restarted as well
@@ -74,7 +64,7 @@ iactor_restart_test2(_Config) ->    % when an iactor is killed, it should be res
     ct_helper:wait(),
     true = is_registered(repo_server),
     NewRepoPid = whereis(repo_server),
-    RepoPid =/= NewRepoPid,
+    RepoPid /= NewRepoPid,
     ok.
 
 iactor_restart_test3(_Config) ->    % when the watchdog dies, the pool has to be restarted and all the iactors in them
@@ -92,6 +82,17 @@ iactor_restart_test3(_Config) ->    % when the watchdog dies, the pool has to be
     true = is_registered(repo_server),
     ok.
 
+iactor_config_test(Config)->    % repo-server needs to be restarted again after shutdown of all the watchdog processes
+    ok = watchdog_server:start_iactor(repo_sup),
+    true = is_registered(repo_server),
+
+    C1 = stop_watchdog_processes(Config),
+    false = is_registered(repo_server),
+
+    _C2 = start_watchdog_processes(C1),
+    true = is_registered(repo_server),
+    ok.
+
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
@@ -103,17 +104,26 @@ is_registered(Name) ->
             true
     end.
 
-%%is_down_received(Ref,Pid) ->
-%%    receive
-%%        {'DOWN', Ref, process, Pid, Reason} ->
-%%            ct_helper:print_var("Message", Reason),
-%%            Reason
-%%    after 1000 ->
-%%        error(exit_timeout)
-%%    end.
-
 kill(RegisteredName) ->
     Pid = whereis(RegisteredName),
     exit(Pid, kill),
     ct_helper:waitms(200),
     ok.
+
+stop_watchdog_processes(Config) ->
+    Sup = ?config(sup,Config),
+    process_flag(trap_exit, true),  % need to trap the exit of the supervisor, otherwise the test-process dies as well!
+    Ref = erlang:monitor(process,Sup),
+    erlang:exit(Sup,shutdown),  % stop the watchdog's supervisor and wait till everything is down, also see: http://stackoverflow.com/questions/21138442/stopping-an-erlang-supervisor
+    receive
+        {'DOWN', Ref, process, Sup, _Reason} ->
+            ok
+    after 1000 ->
+        error(exit_timeout)
+    end,
+    process_flag(trap_exit, false),
+    proplists:delete(sup, Config).
+
+start_watchdog_processes(Config) ->
+    {'ok', Pid} = watchdog_sup:start_link(),
+    [{sup,Pid} | Config].
